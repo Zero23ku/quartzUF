1957dca81788d636ff5fec58947e2073e066635d
Marcelo Cárdenas <cardenas.marcelo@principal.com>
2018-08-21 14:40:26 -0300

    initial commit

---
+++ src/main/java/cl/bennu/quartzuf/quartzuf/QuartzufApplication.java
@@ -0,0 +1,12 @@
+package cl.bennu.quartzuf.quartzuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzufApplication.class, args);
+	}
+}
